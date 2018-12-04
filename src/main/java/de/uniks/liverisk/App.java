684aa87d43b6b092287fd170c70275d7345062b4
Jan Mueller <janmueller3698@gmail.com>
2018-12-04 23:01:48 +0100

    Update App.java

--- src/main/java/de/uniks/liverisk/App.java
+++ src/main/java/de/uniks/liverisk/App.java
@@ -16,7 +16,7 @@ public class App extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         gc = new GameController();
         primaryStage.setScene(SceneBuilder.buildStartScene(primaryStage, gc));
         primaryStage.setResizable(false);
